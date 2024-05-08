325a272b1b6c0b31ac3f831aa8d93a434a9c3c1c
xavierrodrigues <xavier.p.rodrigues@inesctec.pt>
2024-05-08 09:33:48 +0100

    add toke

--- src/main/java/pt/klc/infraspeakintegration/controller/WebhookController.java
+++ src/main/java/pt/klc/infraspeakintegration/controller/WebhookController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
@@ -16,11 +17,18 @@ public class WebhookController {
 
 
     @PostMapping("/api/webhook")
-    public ResponseEntity<?> hookHandling(@RequestBody HashMap<String,Object> body){
+    public ResponseEntity<?> hookHandling(@RequestHeader("Authorization") String authorization, @RequestBody HashMap<String,Object> body){
 
-        log.info(body.toString());
+        if(authorization.equals("Bearer 1ueUgdACc94iWBkuwe7OvHltuLoxJ42BOI2sbzFUdcem38EOQLM98O33DoZ1vpRV")){log.info(body.toString());
 
-        return ResponseEntity.status(200).body(Collections.singletonMap("result","ok"));
+            return ResponseEntity.status(200).body(Collections.singletonMap("result","ok"));}
+
+        else{
+
+            return ResponseEntity.status(401).body(Collections.singletonMap("result","Invalid token provided"));
+        }
+
+
 
     }
 }
